d1c5a83f93a3c3b388897405f952406c5a702f52
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-01 18:12:20 -0500

    Update status code 200 create

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::PinsController < ApplicationController
   before_action :basic_auth
 
   def index
-    render json: Pin.all.order('created_at DESC'), status: 200
+    render json: Pin.all.order('created_at DESC')
   end
 
   def create
     pin = Pin.new(pin_params)
     if pin.save
-      render json: pin, status: 201
+      render json: pin, status: 200
     else
       render json: { errors: pin.errors }, status: 401
     end
