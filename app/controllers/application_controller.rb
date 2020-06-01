c2f14e6b1f2751ab90ec7f5698a2b71a2cb3535e
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-01 18:03:57 -0500

    Update status code basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
      user = User.find_by_email(username)
      if user.api_token == token
        sign_in user
-       head 200
      else
        head 401
      end
