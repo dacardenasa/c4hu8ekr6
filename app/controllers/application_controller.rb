2194abdcbea214143af856d81e3a8c79d9b128cc
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-05 08:47:13 -0500

    Update HTTP API RESPONSE to render JSON STATUS CODE 401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def basic_auth
-   authenticate_or_request_with_http_basic do |username, token|
-     user = User.find_by_email(username)
-     if user.api_token == token
-       sign_in user
-     else
-       head 401
-     end
-   end
+    authenticate_or_request_with_http_basic do |username, token|
+      user = User.find_by_email(username)
+      if user.api_token == token
+        sign_in user
+      else
+        render json: { status: 401 }
+      end
+    end
   end
-
 end
