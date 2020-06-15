8326edbe7c49cbdd413e311d5a1d1ca253c2ae49
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-15 12:12:45 -0500

    Update basic_auth conditional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, token|
-      user = User.find_by_email(username)
-      if user.api_token == token
-        sign_in user
-      else
-        head 401
-      end
-    end
-  end
 end
