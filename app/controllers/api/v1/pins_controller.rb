a62efce7fc5861502d1c746dfb6357ebc0d8187e
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-16 16:57:10 -0500

    Update method Basic auth

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -16,16 +16,6 @@ class Api::V1::PinsController < ApplicationController
 
   private
     def pin_params
-      params.require(:pin).permit(:title, :image_url)
+      params.permit(:title, :image_url)
     end
-  
-  protected
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, token|
-      user = User.find_by_email(username)
-      if user.api_token == token
-        sign_in user
-      end
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  def basic_auth
+    email = request.headers["X-User-Email"]
+    token = request.headers["X-Api-Token"]
+    user = User.find_by_email(email)
+
+    if user.api_token == token
+      sign_in user
+    else
+      head 401
+    end
+    # Authenticate with http_request_basic
+    # authenticate_or_request_with_http_basic do |username, token|
+    #   user = User.find_by_email(username)
+    #   if user.api_token == token
+    #     sign_in user
+    #   end
+    # end
+  end
+
 end
